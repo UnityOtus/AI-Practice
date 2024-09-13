f0ea906d3e0d45f1596bc740694953945acb8852
starkre <gulkin.igor.developer@gmail.com>
2024-09-13 23:12:14 +0600

    Сделал FollowBehaviour

--- Assets/Scripts/Codegen/BlackboardAPI.cs
+++ Assets/Scripts/Codegen/BlackboardAPI.cs
@@ -10,8 +10,75 @@ namespace Atomic.AI
 {
     public static class BlackboardAPI
     {
+        public const int Character = 1; // GameObject : class
+        public const int Target = 2; // GameObject : class
+        public const int StoppingDistance = 3; // float
+        public const int Artem = 4; // Transform : class
 
 
         ///Extensions
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasCharacter(this IBlackboard obj) => obj.HasObject(Character);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static GameObject  GetCharacter(this IBlackboard obj) => obj.GetObject<GameObject >(Character);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool TryGetCharacter(this IBlackboard obj, out GameObject  value) => obj.TryGetObject(Character, out value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void SetCharacter(this IBlackboard obj, GameObject  value) => obj.SetObject(Character, value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool DelCharacter(this IBlackboard obj) => obj.DelObject(Character);
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasTarget(this IBlackboard obj) => obj.HasObject(Target);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static GameObject  GetTarget(this IBlackboard obj) => obj.GetObject<GameObject >(Target);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool TryGetTarget(this IBlackboard obj, out GameObject  value) => obj.TryGetObject(Target, out value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void SetTarget(this IBlackboard obj, GameObject  value) => obj.SetObject(Target, value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool DelTarget(this IBlackboard obj) => obj.DelObject(Target);
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasStoppingDistance(this IBlackboard obj) => obj.HasFloat(StoppingDistance);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float GetStoppingDistance(this IBlackboard obj) => obj.GetFloat(StoppingDistance);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool TryGetStoppingDistance(this IBlackboard obj, out float value) => obj.TryGetFloat(StoppingDistance, out value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void SetStoppingDistance(this IBlackboard obj, float value) => obj.SetFloat(StoppingDistance, value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool DelStoppingDistance(this IBlackboard obj) => obj.DelFloat(StoppingDistance);
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool HasArtem(this IBlackboard obj) => obj.HasObject(Artem);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Transform  GetArtem(this IBlackboard obj) => obj.GetObject<Transform >(Artem);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool TryGetArtem(this IBlackboard obj, out Transform  value) => obj.TryGetObject(Artem, out value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void SetArtem(this IBlackboard obj, Transform  value) => obj.SetObject(Artem, value);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool DelArtem(this IBlackboard obj) => obj.DelObject(Artem);
+
     }
 }
